fd277c45953e76b9299fa0fb6078ddad96e36ee9
Charlie on RohanElf <charlie@elvenware.com>
2017-10-03 21:36:58 -0700

    Add the code

---
+++ spec/test-elf-utils.js
@@ -0,0 +1,14 @@
+/**
+ * Created by charlie on 10/7/15.
+ */
+
+var elfUtils = require('../index').elfUtils;
+
+describe('Elf Utils Suite', function() {
+    'use strict';
+
+    it('expects elfUtils to be truthy', function() {
+        expect(elfUtils).toBeTruthy();
+    });
+
+});
